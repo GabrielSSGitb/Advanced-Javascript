6b0b758302a5f913dad171b219edf35555684ff0
Gabriel SS <gabrielsantosgithubac@gmail.com>
2021-10-26 16:17:55 -0300

    Javascript

--- Exersices/Object/desconstruindo.js
+++ Exersices/Object/desconstruindo.js
@@ -8,12 +8,13 @@ let pessoasinha = {
     },
     
     maisinfo: function() {
-        return `Tem ${this.idade} anos, e é um ${this.cargo}! Suas redes sociais são: \n Facebook: ${this.social.facebook} \n Instagram: ${this.social.Instagram}` 
+        console.log(` Suas Redes Sociais:\n FaceBook: ${this.social.facebook} \n Instagram: ${this.social.Instagram}`); 
     }
 };
 //desconstrução é pegar um dado e transformar em uma váriavel!
 
-let { nome, sobrenome, cargo, idade = 16 } = pessoasinha;
-console.log(nome, sobrenome, cargo, idade); // : Muda o nome da var!
+let { nome, sobrenome, cargo:profissao, idade = 16 } = pessoasinha;
+console.log(nome, sobrenome, profissao, idade); // : Muda o nome da var!
 sobrenome = 'Sikeira';
-console.log(sobrenome, idade);
+console.log(`${nome} ${sobrenome} é um ${profissao}, tem ${idade} anos!`);
+pessoasinha.maisinfo();
