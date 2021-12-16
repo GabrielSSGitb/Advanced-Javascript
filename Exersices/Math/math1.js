513d0e9fa76a0e34356cb9e2988c49b9596e7c67
Gabriel SS <gabrielsantosgithubac@gmail.com>
2021-12-16 14:26:28 -0300

    Testing things

--- Exersices/Math/math1.js
+++ Exersices/Math/math1.js
@@ -1,7 +1,10 @@
 let s = Math.PI;
  //let s2 = Math.floor(5.37); //Para arrendondar!! //Math.round()
+ //arrendonda pra cima:
  let s2 = Math.ceil(3.99);
+  //arrendonda pra baixo:
  let s3 = Math.floor(2.1);
+ // transforma numero negativo em positivo:
  let s5 = Math.abs(-400000);
 console.log(s2);
 console.log(s3);
