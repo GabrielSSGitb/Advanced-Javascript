6c887bf6af86762b9c5f2d1cc1ec953c65afdf56
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-01-18 15:40:11 -0300

    Done

--- Exersices/array/relearning.js
+++ Exersices/array/relearning.js
@@ -1,4 +1,3 @@
-// Make an exercise that has an input to create an array, and use every on it!
 function map() {
     // apply a function to every item in the array:
   let mult = [80,67,98,30,50,90];
@@ -36,4 +35,28 @@ function some() {
     people2 = people.some((item) => item == "Marcao" ? true : false);
     console.log(`Is there "Marcao" in the array?`);
     console.log(people2);
+};
+function find() {
+   //it return the value expecified!
+   let array = [80,67,98,30,50,90];
+   let array2 = [];
+   /*array2 = array.find(function(item, index) {
+       if(item == 80 && index == 0) {
+           return true
+       }else {
+           return false
+       }
+   });*/
+   array2 = array.findIndex((item) => item == 50 ? true : false);
+   console.log(array2);
+};
+function exersice() {
+   let users = [{nome: "Fernanda",idade: 89,cargo: "Frond-end"},{nome: "Gustavo",idade: "38",cargo: "Teacher"},
+   {nome: "Guilherme", idade: 24,cargo: "Back-end" },
+   {nome: "Julia",idade:27, cargo: "Software engineer"}
+   ];
+   let search = [];
+   let input = document.querySelector('#camp').value;
+   search = users.find((item) => item.nome == input ? true : false);
+   console.log(search);
 };
