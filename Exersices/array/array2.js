8cde6a2d1a5377761d05b2c466fe6863bdcf0f49
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-01-19 17:25:42 -0300

    Update array2.js

--- Exersices/array/array2.js
+++ Exersices/array/array2.js
@@ -4,7 +4,15 @@ array.splice(0, 2)
 //Remove tudo: array.splice(1)
  let res = array.concat(array2);
 console.log(res);
-
+let nums = [20,50,40,33,22];
+//nums.sort();
+nums.reverse();
+console.log(nums)
+let soma = 0;
+for(let i = 0; i < nums.length; i++) {
+    soma += nums[i]
+}
+console.log(soma);
 let wallet = 222.8;
 let cal = wallet / 5.47;
 let money = cal.toFixed(2);
