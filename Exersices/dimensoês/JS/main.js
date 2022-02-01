85d3d0e3948210b9333720e5855b069c498c4e5d
Gabriel Santos Silva <gabrielsantosgithubac@gmail.com>
2022-02-01 17:58:58 -0300

    Update main.js

--- Exersices/dimensoês/JS/main.js
+++ Exersices/dimensoês/JS/main.js
@@ -15,7 +15,12 @@ function backtotop() {
         behavior: 'smooth'
     });
 }
- /* function sumirbotão() {
+ /* 
+  the offsetheight and offsetwidth calculates the element´sheight and width.
+  the clientwidth and clientheight doesn´t take the scroll of the element, just the content and padding.
+  the scrollheight and scrollwidth takes only the content with padding and margin, but doesn´t take the scroll
+  
+ function sumirbotão() {
     if(window.scrollY == 0) {
         document.querySelector('.backtotop').style.display= 'none';
     }else  {
