f53be88be90358b24579da9b75af4c16650e10c6
Gabriel Santos Silva <gabrielsantosgithubac@gmail.com>
2022-02-01 12:39:02 -0300

    Number Methods

--- numbers Methods/main.js
+++ numbers Methods/main.js
@@ -6,3 +6,9 @@ console.log(res);
 let n1 = 500.849583835;
 let s = n1.toFixed(2);
 console.log(s);
+// tolocalestring and replace:
+let num1 = 500.55;
+let num2 = 955.55;
+let s2 = num1 * num2;
+s2.toLocaleString().replace(",", ".");
+console.log(s2);
