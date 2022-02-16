f8348e98778ee421c88a55ab0eaf35c67c0867c8
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-02-16 15:49:05 -0300

    Includes, Key & Entries

---
+++ Exersices/includes & repeat/script.js
@@ -0,0 +1,9 @@
+let accounts = ['Gabrielss', 'GabrielMartinsreal', 'Matheustexera124', 'Mariachiquinha444'];
+console.log(accounts.includes('Mariachiquinha444'));
+let text = "USA it is the biggest country in the world!";
+console.log(text.includes('country'));
+let numbers = [9,23,44,99];
+console.log(numbers.includes(44));
+
+let msg = "I love coding ";
+console.log(msg.repeat(30));
