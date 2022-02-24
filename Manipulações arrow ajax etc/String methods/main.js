63a3439ad31645246b54d5ed63f03590d9f5c3f4
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-02-24 16:06:27 -0300

    Deleted Files

--- Manipulações arrow ajax etc/String methods/main.js
+++
@@ -1,8 +0,0 @@
-var nome = 'Aristoteles kfmsdlfld bouser kidjskfk index';
-let resultado = '';
- if(nome.indexOf('k') > 2) {
-     console.log('achou')
- }else {
-     console.log('não achou!')
- }
-console.log(resultado);
