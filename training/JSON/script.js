a75ca1ccd08817bd344b6c70c8f92985a198d9d0
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-03-05 12:43:09 -0300

    Working on the Error part

--- training/JSON/script.js
+++ training/JSON/script.js
@@ -10,20 +10,52 @@ class processdata {
      return this.verify(savedata);
     };
     verify(savedata) {
-     console.log(savedata);
-     if(savedata.name == '') {
-         return this.msg();
+        var box = document.querySelector('.hiddenmsg');
+        var h3 = document.createElement('h3');
+        var p = document.createElement('p');
+        var span = document.createElement('span');
+        console.log(savedata);
+     if(savedata.name == '' || savedata.lastname == '') {
+         h3.textContent= `Something Went Wrong!!!`;
+         p.textContent= `Please, Inform your name!`;
+         span.textContent= `x`;
+         return this.msg(box, h3, p, span);
+     }else if(savedata.birth == '') {
+        h3.textContent= `Something went Wrong!!!`;
+        p.textContent= `Please, inform your birth to continue!`;
+        span.textContent= `x`;
+        return this.msg(box, h3, p, span);
+     }else if(savedata.cpf == '' ||  savedata.cpf > 11 || savedata.cpf < 11) {
+        h3.textContent= `Something went Wrong!!!`;
+        p.textContent= `CPF camp is invalid!`;
+        span.textContent= `x`;
+        return this.msg(box, h3, p, span);
+     }else if(savedata.rg == '' || savedata.rg > 8 || savedata.rg < 8) {
+        h3.textContent= `Something went Wrong!!!`;
+        p.textContent= `RG camp is invalid!`;
+        span.textContent= `x`;
+        return this.msg(box, h3, p, span);
+     }else if(savedata.credit == '' || savedata.credit > 16) {
+        h3.textContent= `Something went Wrong!!!`;
+        p.textContent= `Please, inform your credit card!`;
+        span.textContent= `x`;
+        return this.msg(box, h3, p, span);
      }
     };
-    msg() {
-      var box = document.querySelector('.hiddenmsg');
+    msg(box, h3, p, span) {
       box.classList.remove("hiddenmsg");
       box.classList.add("msg");
-      document.querySelector('#pm').innerHTML= `Please Inform your name!`;
-      document.querySelector('#closebutton').addEventListener('click', () => {
-          box.classList.remove("msg");
-          box.classList.add("hiddenmsg");
-      });
+      box.appendChild(h3);
+      box.appendChild(p);
+      box.appendChild(span);
+      return this.closemsg(box, h3, p, span)
+    };
+    closemsg(box, h3, p, span) {
+      span.addEventListener('click', () => {
+        box.removeChild(h3);
+        box.removeChild(p);
+        window.document.body.removeChild(box);
+      })
     };
 };
 var Processdata = new processdata;
