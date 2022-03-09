5e96422ce43a2acded3c8b9ea46255bd5b10efca
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-03-09 15:54:02 -0300

    Create callbacks.js

---
+++ Exersices/Requisições e Assincronismo/callbacks.js
@@ -0,0 +1,7 @@
+function alarm() {
+    console.log("Wake up pal, you gotta go to work!");
+};
+let nome = "Gabriel";
+setTimeout(alarm, 3000);
+let lastname = "Santos";
+console.log('Fullname = ', nome+' '+lastname);
