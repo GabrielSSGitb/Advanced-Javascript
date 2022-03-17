30795bf983ab6ff61ca01fbd6a08eefd10887f36
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-03-17 15:12:47 -0300

    Update fetch1.js

--- Exersices/Requisições e Assincronismo/Fetch/fetch1.js
+++ Exersices/Requisições e Assincronismo/Fetch/fetch1.js
@@ -1,9 +1,13 @@
 function loaposts() {
-  fetch('https://jsonplaceholder.typicode.com/posts'). 
-   then((result) => {
-       console.log(result);
-   }).catch((erro) => {
-        console.log("ERROR");
-    })
+ document.getElementById('posts').innerHTML="Loading....";
+  fetch('https://jsonplaceholder.typicode.com/comments')
+  .then((result) => {
+    return result.json(); //This is a Promise!!!
+  })
+  .then((json) => {
+    document.getElementById('posts').innerHTML=`${json[0].email} e outras ${json.length - 1} pessoas, comentarão no seu site!`;
+    console.log(json)
+  })
+  .catch((error) => console.log('ERROR'))
   ;
 };
