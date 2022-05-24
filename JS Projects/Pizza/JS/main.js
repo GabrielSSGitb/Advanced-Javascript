9655edc531be136bcd5d01407b96856e7c1e682b
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-05-24 18:03:51 -0300

    Started building the containers with JS

--- JS Projects/Pizza/JS/main.js
+++ JS Projects/Pizza/JS/main.js
@@ -1,13 +1,29 @@
-import {pizzas} from "./pizzas.js";
+import {pizzaslist} from "./pizzas.js";
 class pizza {
    constructor() {
      this.id= 0;
    };
+   start() {
+    for(let i in pizzaslist) {
+      this.id+= 1;
+      pizzaslist[i].id= this.id;
+    };
+    return this.create();
+   };
    create() {
-     for(let i in pizzas) {
-        this.id+= 1;
-     }
+    for(let i in pizzaslist) {
+      let section = document.querySelector('#content');
+      let container = document.createElement('div');
+      let h2 = document.createElement('h2');
+      let price = document.createElement('p');
+      let text = document.createElement('p');
+      let button = document.createElement('button');
+      container.classList.add('pizza-container');
+      price.classList.add('price');
+      text.classList.add('text');
+      section.appendChild(container);
+    };
    };
 };
 var Pizza = new pizza;
-Pizza.create();
+Pizza.start();
