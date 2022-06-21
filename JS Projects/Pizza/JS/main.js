419439c079361a1041f2b6fd793e960577a2a238
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-06-21 19:25:34 -0300

    Almost done with the structure

--- JS Projects/Pizza/JS/main.js
+++ JS Projects/Pizza/JS/main.js
@@ -8,7 +8,7 @@ class pizza {
       this.id+= 1;
       pizzaslist[i].id= this.id;
     };
-    return this.create();
+    return this.events();
    };
    create() {
     for(let i in pizzaslist) {
@@ -18,19 +18,30 @@ class pizza {
       var h2 = document.createElement('h2');
       var price = document.createElement('p');
       var text = document.createElement('p');
+      var button = document.createElement('button');
       container.classList.add('pizza-container');
       price.classList.add('price');
-      text.classList.add('text')
+      text.classList.add('text');
       img.src= pizzaslist[i].img;
       h2.textContent= pizzaslist[i].name;
       price.textContent= `R$ ${pizzaslist[i].price}`;
-      text.textContent= this.text;
+      text.textContent= "This Pizza it´s insane because it´s pretty damm good, incredible, buy it you are not gonna regret it!";
+      button.textContent= `Add in the count`
       container.appendChild(img);
       container.appendChild(h2);
       container.appendChild(price);
       container.appendChild(text);
+      container.appendChild(button);
       section.appendChild(container);
     };
+   };
+   events() {
+     document.querySelector('#link').addEventListener('click', () => this.sidebar());
+     return this.create();
+   };
+   sidebar() {
+      document.querySelector('aside').style.display='block';
+      document.querySelector('section').style.maxWidth='80vw';
    };
 };
 var Pizza = new pizza;
