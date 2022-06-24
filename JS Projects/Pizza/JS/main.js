643a6b7705f6bed7ae5d5a7e5dcc44e499ebe8c7
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-06-24 12:26:59 -0300

    Tried to solve a puzzle

--- JS Projects/Pizza/JS/main.js
+++ JS Projects/Pizza/JS/main.js
@@ -2,6 +2,7 @@ import {pizzaslist} from "./pizzas.js";
 class pizza {
    constructor() {
      this.id= 0;
+     this.buttonid = 0;
    }
    start() {
     for(let i in pizzaslist) {
@@ -12,6 +13,8 @@ class pizza {
    };
    create() {
     for(let i in pizzaslist) {
+      this.buttonid+= 1;
+
       var section = document.querySelector('#content');
       var container = document.createElement('div');
       var img = document.createElement('img');
@@ -19,14 +22,18 @@ class pizza {
       var price = document.createElement('p');
       var text = document.createElement('p');
       var button = document.createElement('button');
+
       container.classList.add('pizza-container');
       price.classList.add('price');
       text.classList.add('text');
+      button.classList.add(this.buttonid);
+      this.list(button);
+
+      button.textContent= 'Add in the List';
       img.src= pizzaslist[i].img;
       h2.textContent= pizzaslist[i].name;
       price.textContent= `R$ ${pizzaslist[i].price}`;
       text.textContent= "This Pizza it´s insane because it´s pretty damm good, incredible, buy it you are not gonna regret it!";
-      button.textContent= `Add in the count`
       container.appendChild(img);
       container.appendChild(h2);
       container.appendChild(price);
@@ -42,6 +49,9 @@ class pizza {
    sidebar() {
       document.querySelector('aside').style.display='block';
       document.querySelector('section').style.maxWidth='80vw';
+   };
+   list(button) {
+    
    };
 };
 var Pizza = new pizza;
