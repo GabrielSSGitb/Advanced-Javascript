3cf1b84fb08b908e42d4bf473d32e781db9faf10
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-07-25 14:51:26 -0300

    Started recreating the structure

--- JS Projects/Pizza/JS/main.js
+++ JS Projects/Pizza/JS/main.js
@@ -3,53 +3,30 @@ class pizza {
    constructor() {
      this.id= 0;
    }
-   start() {
-    for(let i in pizzaslist) {
-      this.id+= 1;
-      pizzaslist[i].id= this.id;
-    };
-    return this.events();
-   };
    create() {
-    for(let i in pizzaslist) {
-      var section = document.querySelector('#content');
-      var container = document.createElement('div');
-      var img = document.createElement('img');
-      var h2 = document.createElement('h2');
-      var price = document.createElement('p');
-      var text = document.createElement('p');
-      var button = document.createElement('button');
+     pizzaslist.map((item, index) => {
+      let section = document.querySelector('#content');
+      let container = document.createElement('div');
+      let img = document.createElement('img');
+      let h2 = document.createElement('h2');
+      let text = document.createElement('p');
+      let btn = document.createElement('button');
+
+      img.src= item.img;
+      h2.textContent= item.name;
+      text.textContent= "This Pizza it´s insane because it´s pretty damm good, incredible, buy it you are not gonna regret it!";
+      btn.textContent= "Add to menu";
 
       container.classList.add('pizza-container');
-      price.classList.add('price');
       text.classList.add('text');
-      button.classList.add(pizzaslist[i].id);
 
-      img.src= pizzaslist[i].img;
-      h2.textContent= pizzaslist[i].name;
-      price.textContent= `R$ ${pizzaslist[i].price}`;
-      text.textContent= "This Pizza it´s insane because it´s pretty damm good, incredible, buy it you are not gonna regret it!";
-      button.textContent= `Add to menu`;
       container.appendChild(img);
       container.appendChild(h2);
-      container.appendChild(price);
       container.appendChild(text);
-      container.appendChild(button);
+      container.appendChild(btn);
       section.appendChild(container);
-      return this.buttonsevent();
-    };
-   };
-   events() {
-     document.querySelector('#link').addEventListener('click', () => this.sidebar());
-     return this.create();
-   };
-   sidebar() {
-      document.querySelector('aside').style.display='block';
-      document.querySelector('section').style.maxWidth='80vw';
-   };
-   buttonsevent() {
-    
+     });  
    };
 };
 var Pizza = new pizza;
-Pizza.start();
+Pizza.create();
