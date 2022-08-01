024ea475047c5748adc0e190a4dce8afd57cd67a
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-08-01 15:46:05 -0300

    Added events for buttons and started the navbar

--- JS Projects/Pizza/JS/main.js
+++ JS Projects/Pizza/JS/main.js
@@ -1,11 +1,17 @@
 import {pizzaslist} from "./pizzas.js";
+const get = (el) => document.querySelector(el);
+const getall = (els) => document.querySelectorAll(els);
 class pizza {
    constructor() {
-     this.id= 0;
-   }
+    this.id = 0;
+    for(let i in pizzaslist) {
+     this.id++;
+     pizzaslist[i].id = this.id;
+    };
+   };
    create() {
-     pizzaslist.map((item, index) => {
-      let section = document.querySelector('#content');
+     pizzaslist.map((item) => {
+      let section = get('#content');
       let container = document.createElement('div');
       let img = document.createElement('img');
       let h2 = document.createElement('h2');
@@ -19,6 +25,11 @@ class pizza {
 
       container.classList.add('pizza-container');
       text.classList.add('text');
+      btn.classList.add(item.id);
+      btn.addEventListener('click', () => {
+        const elselected = [{name: item.name, price: item.price}];
+         return this.opennav(elselected);
+      });
 
       container.appendChild(img);
       container.appendChild(h2);
@@ -27,6 +38,25 @@ class pizza {
       section.appendChild(container);
      });  
    };
+   opennav(elselected) {
+      get('aside').style.display='block';
+      get('section').style.maxWidth= '80vw';
+
+      var menulist = document.createElement('div');
+      var pizzaname = document.createElement('p');
+      var pricelist = document.createElement('p');
+      var plussign = document.createElement('i');
+      var minussign = document.createElement('i');
+      var asidebtn = document.createElement('button');
+
+      menulist.classList.add('list');
+      pizzaname.classList.add('pizzaname');
+      pricelist.classList.add('pricelist');
+      plussign.classList.add('fa-solid fa-plus');
+      minussign.classList.add('fa-solid fa-minus');
+
+      get('aside').appendChild(menulist);
+  };
 };
 var Pizza = new pizza;
 Pizza.create();
