3dbcf4e08ce4c921c481c4c95660417cb6dc8690
Gabriel SS <gabrielsantosgithubac@gmail.com>
2023-01-14 13:34:58 -0300

    Update main.js

--- JS Projects/new calculator/JS/main.js
+++ JS Projects/new calculator/JS/main.js
@@ -1,11 +1,22 @@
 class calculator {
     insert(num) {
-        const number = document.querySelector('#showvalue').value;
-        document.querySelector('#showvalue').value= number + num;
+        const number = document.querySelector('#shown1').value;
+        document.querySelector('#shown1').value= number + num;
+    };
+    insertfunc(func) {
+        const todo = document.querySelector('#showtext').value;
+        document.querySelector('#showtext').value= func;
+
+        if(func == '÷') {
+    
+        }else {
+            alert('error')
+        }
     };
     clear() {
-        document.querySelector('#showvalue').value= '';
+        document.querySelector('#shown1').value= '';
         document.querySelector('#showresult').value= '';
+        document.querySelector('#showtext').value= '';
     };
 };
 var Calculator = new calculator;
