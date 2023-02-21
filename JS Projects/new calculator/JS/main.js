9d1a16ab72ca6e6194706cf3a31c83f5a5fe57b6
Gabriel SS <gabrielsantosgithubac@gmail.com>
2023-02-21 12:47:48 -0300

    Did some progress

--- JS Projects/new calculator/JS/main.js
+++ JS Projects/new calculator/JS/main.js
@@ -1,20 +1,35 @@
 class calculator {
     insert(num) {
-        const number = document.querySelector('#shown1').value;
-        document.querySelector('#shown1').value= number + num;
+        const showtext = document.querySelector('#showtext').value;
+
+        while(showtext == '') {
+            const number = document.querySelector('#shown1').value;
+            document.querySelector('#shown1').value = number + num;
+            showtext++
+        };
+        while(showtext != '') {
+            const number2 = document.querySelector('#shown2').value;
+            document.querySelector('#shown2').value = number2 + num;
+            showtext++
+        };
     };
     insertfunc(func) {
         const todo = document.querySelector('#showtext').value;
         document.querySelector('#showtext').value= func;
-
-        if(func == '÷') {
-    
+        this.process(func);
+    };
+    process(func) {
+        const num1 = document.querySelector('#shown1').value;
+        const num2 = document.querySelector('#shown2').value;
+         if(func == "+") {
+            document.querySelector('#showresult').value = num1 + num2;
         }else {
-            alert('error')
+            alert('error');
         }
     };
     clear() {
         document.querySelector('#shown1').value= '';
+        document.querySelector('#shown2').value= '';
         document.querySelector('#showresult').value= '';
         document.querySelector('#showtext').value= '';
     };
