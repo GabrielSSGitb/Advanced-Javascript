e7bc247fe6e428a5a3e503218c8fc55d8f19d6aa
Gabriel SS <gabrielsantosgithubac@gmail.com>
2023-11-30 20:32:13 -0300

    Update main.js

--- JS Projects/new calculator/JS/main.js
+++ JS Projects/new calculator/JS/main.js
@@ -19,13 +19,18 @@ class calculator {
         this.process(func);
     };
     process(func) {
-        const num1 = document.querySelector('#shown1').value;
-        const num2 = document.querySelector('#shown2').value;
-         if(func == "+") {
+            let num1 = document.querySelector('#shown1').value;
+            let num2 = document.querySelector('#shown2').value;
+            document.querySelector('#equal').addEventListener('click',() => {
+                let n1 = Number(num1);
+                let n2 = Number(num2);
+                console.log(n1, n2);
+            });
+       /* if(func == "+") {
             document.querySelector('#showresult').value = num1 + num2;
         }else {
             alert('error');
-        }
+        }*/
     };
     clear() {
         document.querySelector('#shown1').value= '';
